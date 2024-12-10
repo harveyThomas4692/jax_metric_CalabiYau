a72dc016844ab3f7ccc8c11e3568395f9ba392c0
Thomas Harvey <thomas.harvey@live.co.uk>
2024-12-10 12:54:41 -0500

    Pullback
    I've included a function for computing the pullback - this needs checking though!

--- metric_functions/pullback.py
+++ metric_functions/pullback.py
@@ -4,11 +4,12 @@ from jax import jit
 
 jit_setdiff1d = jit(jnp.setdiff1d, static_argnames=['size'])
 
-def pullback(pt, poly):
+def pullback(pt, projective_factors, poly):
     """
     Computes the pullback matrix for a given point, and polynomial.
     Args:
         pt (array-like): The point at which to evaluate the pullback.
+        projective_factors (list): A list of integers representing the projective factors of the Calabi-Yau manifold.
         poly (function): The polynomial function for which the gradient is computed.
     Returns:
         jnp.ndarray: The pullback matrix.
@@ -16,24 +17,25 @@ def pullback(pt, poly):
     
     dPoly = jax.grad(poly,holomorphic=True)
     dP = dPoly(pt)
-    pt_ones = jnp.where(jnp.real(pt) == 1.0)[0]
+    scales = len(projective_factors)
+    pt_ones = jnp.argsort(jnp.abs(pt))[-scales:]
     args = jnp.argsort(jnp.abs(dP))
 
     # Delete the specified rows
-    rm_index = jnp.setdiff1d(args, pt_ones)[0]
+    rm_index = jit_setdiff1d(args, pt_ones,size=1)[0]
 
 
     chain = jnp.outer(dP, 1./dP)  # This has indices pb_b^a = dp^a / dp_b
 
     # Update the desired column with zeros and ones
-    pb = jnp.eye(len(pt))
+    pb = jnp.eye(len(pt),dtype=pt.dtype)
     pb = pb.at[:,pt_ones].set(jnp.zeros((len(pt),len(pt_ones))))
     pb = pb.at[:,rm_index].set(chain[:,rm_index])
 
 
-    keep_indices = jnp.setdiff1d(jnp.arange(len(pt)),jnp.append(pt_ones, rm_index))
+    keep_indices = jit_setdiff1d(jnp.arange(len(pt)),jnp.append(pt_ones, rm_index),size=len(pt)-1-len(pt_ones))
     pb = pb[keep_indices, :]
 
     return pb
 
-#pullback = jit(pullback,static_argnums=(2,))
+pullback = jit(pullback,static_argnums=(2,))
