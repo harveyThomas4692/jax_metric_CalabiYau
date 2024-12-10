1d210738bad40f5e126b5eb3bcbb18aa946910a3
Thomas Harvey <thomas.harvey@live.co.uk>
2024-12-10 11:54:39 -0500

    Pullback
    Included code for computing pullbacks to an arbitary CICY

--- metric_functions/pullback.py
+++ metric_functions/pullback.py
@@ -4,11 +4,36 @@ from jax import jit
 
 jit_setdiff1d = jit(jnp.setdiff1d, static_argnames=['size'])
 
-def pullback(pt, projective_factors, poly):
-    '''
-    Get the pullback map at a point on a CY manifold.
-    '''
-    pb = 1
+def pullback(pt, poly):
+    """
+    Computes the pullback matrix for a given point, and polynomial.
+    Args:
+        pt (array-like): The point at which to evaluate the pullback.
+        poly (function): The polynomial function for which the gradient is computed.
+    Returns:
+        jnp.ndarray: The pullback matrix.
+    """
+    
+    dPoly = jax.grad(poly,holomorphic=True)
+    dP = dPoly(pt)
+    pt_ones = jnp.where(jnp.real(pt) == 1.0)[0]
+    args = jnp.argsort(jnp.abs(dP))
+
+    # Delete the specified rows
+    rm_index = jnp.setdiff1d(args, pt_ones)[0]
+
+
+    chain = jnp.outer(dP, 1./dP)  # This has indices pb_b^a = dp^a / dp_b
+
+    # Update the desired column with zeros and ones
+    pb = jnp.eye(len(pt))
+    pb = pb.at[:,pt_ones].set(jnp.zeros((len(pt),len(pt_ones))))
+    pb = pb.at[:,rm_index].set(chain[:,rm_index])
+
+
+    keep_indices = jnp.setdiff1d(jnp.arange(len(pt)),jnp.append(pt_ones, rm_index))
+    pb = pb[keep_indices, :]
+
     return pb
 
-pullback = jit(pullback,static_argnums=(2,))
+#pullback = jit(pullback,static_argnums=(2,))
